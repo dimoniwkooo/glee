d3c1fe188955f6f98d12ad5b3c368947b99cdb27
Dmitry Chernyakevich <39997792+dimoniwkooo@users.noreply.github.com>
2021-03-24 13:17:03 +0300

    fix home page and add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,47 @@ $(function () {
     dots: true,
     arrows: false,
     fade: true,
-    // autoplay: true,
-    // autoplaySpeed: 5000,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  })
+
+  $('.partners__items').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  })
+
+  $('.header__btn').on('click', function () {
+    $('.menu__items').toggleClass('active')
+    $('.over').toggleClass('active')
   })
 
   mixitup('.products__items', {
